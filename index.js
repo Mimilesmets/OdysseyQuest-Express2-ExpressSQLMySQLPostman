18e610d28b8a190289914b629c89492a7ba7177b
Emilie Smets <emiliesmets@MacBook-Pro-de-Emilie.local>
2019-08-27 14:58:52 +0200

    adapt index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require ('express');
 const app = express();
+const port = 3000;
+const connection = require ('./conf');
 
-app.get ('/api/movies', function (req, res) {
-    res.send('movies')
-})
-app.get ('/api/movies/names', function (req, res){
-    res.send('movies names')
-})
+app.get('/api/movie', (req, res) => {
+connection.query('SELECT * from movie', (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving movies');
+    } else {
+      res.json(results);
+    }
+    
+  });
+ 
+ });
+ 
+ 
+ app.get('/api/movie/name', (req, res) => {
+  let sql = 'SELECT name  from movie';
+  
+  connection.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving movies');
+    } else {
+      res.json(results);
+    }
+    
+  });
+ });
